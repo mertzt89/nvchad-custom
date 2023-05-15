40cbe7e3b4d5abc4847c19b46b4c6d3a5bc1efdb
Timothy Mertz <mertzt89@gmail.com>
2023-05-15 00:35:21 -0500

    Add format on save support

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -1,10 +1,23 @@
-local on_attach = require("plugins.configs.lspconfig").on_attach
+local super_on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
 
 -- if you just want default config for the servers then put them in a table
 local servers = { "html", "cssls", "tsserver", "clangd" }
+
+local on_attach = function(client, bufnr)
+  vim.api.nvim_create_autocmd("BufWritePre", {
+    buffer = bufnr,
+    callback = function()
+      -- if require("lib.project").config.lsp.format_on_save then
+      vim.lsp.buf.format { async = false }
+      -- end
+    end,
+  })
+
+  super_on_attach(client, bufnr)
+end
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
@@ -13,7 +26,32 @@ for _, lsp in ipairs(servers) do
   }
 end
 
+-- Copy of core config to hook on_attach
+lspconfig.lua_ls.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { "vim" },
+      },
+      workspace = {
+        library = {
+          [vim.fn.expand "$VIMRUNTIME/lua"] = true,
+          [vim.fn.expand "$VIMRUNTIME/lua/vim/lsp"] = true,
+          [vim.fn.stdpath "data" .. "/lazy/extensions/nvchad_types"] = true,
+          [vim.fn.stdpath "data" .. "/lazy/lazy.nvim/lua/lazy"] = true,
+        },
+        maxPreload = 100000,
+        preloadFileSize = 10000,
+      },
+    },
+  },
+}
+
 lspconfig.pylsp.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
   settings = {
     pylsp = {
       configurationSources = { "flake8" },
